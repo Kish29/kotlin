86c7aed6bcc33339cc9d438ede7c2d4c8ebdbd1b
Denis.Zharkov <denis.zharkov@jetbrains.com>
2021-04-27 13:16:06 +0300

    FIR: Refine makesSenseToBeDefinitelyNotNull
    Make it work just the same as the analogue from FE 1.0
    This change is necessary since many tests start failing after
    we began reporting diagnostics after call completion

--- compiler/fir/resolve/src/org/jetbrains/kotlin/fir/types/ConeTypeContext.kt
+++ compiler/fir/resolve/src/org/jetbrains/kotlin/fir/types/ConeTypeContext.kt
@@ -431,7 +431,7 @@ interface ConeTypeContext : TypeSystemContext, TypeSystemOptimizationContext, Ty
     }
 
     private fun FirTypeParameterSymbol.allBoundsAreNullable(): Boolean {
-        return fir.bounds.all { it.coneType.isMarkedNullable }
+        return fir.bounds.all { it.coneType.isNullableType() }
     }
 
     private fun TypeConstructorMarker.toFirRegularClass(): FirRegularClass? {
