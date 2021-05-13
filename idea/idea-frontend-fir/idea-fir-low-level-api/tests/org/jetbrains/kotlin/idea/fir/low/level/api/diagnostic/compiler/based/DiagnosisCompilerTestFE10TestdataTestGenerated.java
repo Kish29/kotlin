94a06fd20355272e15c74e7c86dcfb51077b9b64
Denis.Zharkov <denis.zharkov@jetbrains.com>
2021-05-13 15:11:20 +0300

    Minor. Regenerate tests

--- idea/idea-frontend-fir/idea-fir-low-level-api/tests/org/jetbrains/kotlin/idea/fir/low/level/api/diagnostic/compiler/based/DiagnosisCompilerTestFE10TestdataTestGenerated.java
+++ idea/idea-frontend-fir/idea-fir-low-level-api/tests/org/jetbrains/kotlin/idea/fir/low/level/api/diagnostic/compiler/based/DiagnosisCompilerTestFE10TestdataTestGenerated.java
@@ -11480,6 +11480,11 @@ public class DiagnosisCompilerTestFE10TestdataTestGenerated extends AbstractDiag
                     runTest("compiler/testData/diagnostics/tests/inference/completion/equalityConstraintUpstairs.kt");
                 }
 
+                @TestMetadata("errorAfterCompletion.kt")
+                public void testErrorAfterCompletion() throws Exception {
+                    runTest("compiler/testData/diagnostics/tests/inference/completion/errorAfterCompletion.kt");
+                }
+
                 @TestMetadata("flexibleType.kt")
                 public void testFlexibleType() throws Exception {
                     runTest("compiler/testData/diagnostics/tests/inference/completion/flexibleType.kt");
